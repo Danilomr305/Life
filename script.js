0a2a10da00b438ad92116e8e1dcd9a25fd2092c7
Danilomr305 <danilomr305@gmail.com>
2023-05-15 16:36:43 -0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+function mudouTamanho() {
+    if (window.innerWidth >=768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
+function clickMenu(){
+           
+    if (itens.style.display == 'block') {   
+        itens.style.display = 'none'
+    }else {
+        itens.style.display = 'block'
+    }
+}
+
 function calcular() {
     let data = new Date()
     let ano = data.getFullYear()
     let n1 = document.getElementById ('txtano')
     let res = document.getElementById ('res')
-    if (n1.ariaValueMax.length == 0 || Number (n1.value) > ano) {
+    if (n1.value.length == 0 || Number (n1.value) > ano) {
         window.alert('verifique os dados e tente novamente!')
-    }else {
+    } else {
         let anim = document.getElementsByName('rani')
         let idade = ano - Number(n1.value)
-        res.innerHTML = `O seu Dog tem ${idade}`
+        res.innerHTML = `O seu Dog tem ${idade}!`
         let animal = ''
         let img = document.createElement('img')
         img.setAttribute('id', 'foto')
@@ -17,20 +34,23 @@ function calcular() {
             if (idade >= 0 && idade < 2) {
                 /*img:cachorro-bebe*/
                 img.setAttribute('src', 'bebe.jpg')
-            }else if (idade < 2) {
+            }else if (idade > 2) {
                 /*img:cachorro-criança*/
                 img.setAttribute('src', 'criança.jpg')
-            }else if (idade < 5) {
+            }else if (idade > 5) {
                 /*img:cachorro-adolescente*/
                 img.setAttribute('src', 'adolescente.jpg')
-            }else if (idade < 8 ) {
+            }else if (idade > 8 ) {
                 /*img:cachorro-adulto*/
                 img.setAttribute('src', 'adulto.jpg')
-            }else if (idade < 11) {
+            }else if (idade > 11) {
                 /*img:cachorro-idoso*/
                 img.setAttribute ('src', 'idoso.jpg')
             }
         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `O seu  ${animal} tem ${idade} anos!`
+        res.appendChild (img)
 
     }
 }
